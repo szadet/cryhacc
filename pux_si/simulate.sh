f02c827710d2d8862c3a2fbb0ad34019c912033a
tomasz@tomasz-H110M-S2H <szade.tomasz@gmail.com>
2018-06-29 21:11:10 +0200

    Modified simulation script.

--- pux_si/simulate.sh
+++ pux_si/simulate.sh
@@ -4,7 +4,7 @@ echo "PROJECT NAME IS ${PWD##*/}"
 
 if [ -x ./sim_bin/${PWD##*/} ]; then
   vvp "./sim_bin/${PWD##*/}"
-#  gtkwave ./sim_bin/test.vcd wave.sav
+  #gtkwave ./sim_bin/test.vcd wave.sav
 else
   echo "Project not compiled!"
 fi
